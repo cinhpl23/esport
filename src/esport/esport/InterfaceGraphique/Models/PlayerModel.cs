40994849b31765ed913862cd3a84b9d08411a219
Roxane Crépeaux <crepeaux.roxane@gmail.com>
2024-03-25 14:55:14 +0100

    Add player model

---
+++ src/esport/esport/InterfaceGraphique/Models/PlayerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esport.InterfaceGraphique.Models
+{
+    public class PlayerModel
+    {
+        public string Name { get; set; }
+        public string Pseudo { get; set; }
+        public string TeamName { get; set; }
+    }
+}
