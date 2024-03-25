284ff85453a670db0580f641fcb384768ff3782b
cinhpl <101573729+cinhpl@users.noreply.github.com>
2024-03-25 13:05:01 +0100

    Add Team and TeamService

---
+++ src/esport/esport/Business/Services/TeamService.cs
@@ -0,0 +1,36 @@
+﻿using esport.Business.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esport.Business.Services
+{
+    internal class TeamService
+    {
+        private Dictionary<int, Team> _teams = new Dictionary<int, Team>();
+
+        public void AddTeam(int id, string name)
+        {
+            Team team = new()
+            {
+                Id = id,
+                Name = name
+            };
+            _teams.Add(id, team);
+        }
+
+        public Team GetTeam(int id) 
+        { 
+            if (_teams.ContainsKey(id))
+            {
+                return _teams[id];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- src/esport/esport/MainPage.xaml.cs
+++ src/esport/esport/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace esport
+﻿using esport.Business.Services;
+
+namespace esport
 {
     public partial class MainPage : ContentPage
     {
@@ -11,6 +13,9 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
+            TeamService teamService = new TeamService();
+            teamService.AddTeam(1, "Team1");
+
             count++;
 
             if (count == 1)
