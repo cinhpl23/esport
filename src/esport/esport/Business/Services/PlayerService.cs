99c1a0c5c6ceee617ef21cf4a7aee91003644d5d
Roxane Crépeaux <crepeaux.roxane@gmail.com>
2024-03-25 13:30:25 +0100

    Add player service with AddPlayer method

---
+++ src/esport/esport/Business/Services/PlayerService.cs
@@ -0,0 +1,22 @@
+﻿using esport.Business.Entites;
+
+namespace esport.Business.Services
+{
+    public class PlayerService
+    {
+        private Dictionary<int, Player> _players = new Dictionary<int, Player>();
+
+        public void AddPlayer(string name, string pseudo, int idTeam)
+        {
+            Player player = new Player()
+            {
+                Id = _players.Count + 1,
+                Name = name,
+                Pseudo = pseudo,
+                IdTeam = idTeam,
+                MatchWin = 0
+            };
+            _players.Add(player.Id, player);
+        }
+    }
+}
