28d5e70cfc6af4a403c4d424620ef7da557837b0
cinhpl23 <cindy.houpline@my-digital-school.org>
2024-03-28 16:22:17 +0100

    Add Rank interface structure

---
+++ src/esport/esport/InterfaceGraphique/Views/RankView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Collections.ObjectModel;
+using esport.Business.Entites;
+using esport.InterfaceGraphique.ViewModels;
+using Microsoft.Maui.Controls;
+
+namespace esport.Views
+{
+    public partial class RankView : ContentPage
+    {
+        public RankView()
+        {
+            InitializeComponent();
+        }
+    }
+}
